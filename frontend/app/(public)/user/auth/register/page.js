abc3c231c7cec91fab3517558d0797e7f29497e8
Raharinda <raharindanadhif@gmail.com>
2026-02-14 12:27:22 +0700

    hover navbar

--- frontend/app/(public)/user/auth/register/page.js
+++ frontend/app/(public)/user/auth/register/page.js
@@ -38,6 +38,7 @@ export default function RegisterPage() {
                 <label>Nomor Telepon</label>
                 <input
                     {...register('Handphone')}
+                    type='number'
                     placeholder='08xxxxxxxxxx'
                     className='w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
                 />
